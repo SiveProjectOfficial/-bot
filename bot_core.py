b836d46aed1881b13813da4d220366afeb5d6045
Lalala <1214yt.na722@gmail.com>
2026-02-22 22:40:05 +0900

    Update bot_core.py

--- bot_core.py
+++ bot_core.py
@@ -5,35 +5,38 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 def get_wiki_material():
-    # ★ここだけ書き換えたよ！Wikiに行かずに身内のファイルを読み取る
     try:
         with open("Word.txt", "r", encoding="utf-8") as f:
             return f.read()
     except:
-        return "UTAU 楽曲制作 重音テト 正弦波 歌声 バニラアイス"
+        # 正弦波くんの名前やテトさんは抜いて、バックアップもクリーンに！
+        return "楽曲制作 物理演算 エディタ ノート 歌声合成 エンジン wavファイル ツール"
 
 def make_markov_text(source_text):
-    words = re.findall(r'[ぁ-んァ-ヶー一-龠]+|[a-zA-Z]+', source_text)
+    # ★ここを修正！ ひらがな、カタカナ、漢字、英数字を1文字から全部拾うようにしたよ！
+    # これで「を」や「が」も全部マルコフ連鎖の材料になる！！
+    words = re.findall(r'[ぁ-ん]|[ァ-ヶー]|[一-龠]+|[a-zA-Z0-9]+', source_text)
+    
     if len(words) < 10: return "波が静かだ"
+    
     markov = {}
     for i in range(len(words) - 2):
         key = (words[i], words[i+1])
         if key not in markov: markov[key] = []
         markov[key].append(words[i+2])
+    
     try:
         curr = random.choice(list(markov.keys()))
         res = list(curr)
-        for _ in range(15):
+        # 文の長さもイーロンに怒られない程度（15〜20語くらい）に調整
+        for _ in range(18):
             if curr in markov:
                 nxt = random.choice(markov[curr])
                 res.append(nxt)
                 curr = (res[-2], res[-1])
             else: break
-        return "".join(res)
-    except: return "不思議な波"
 
 def sine_wave_bot():
-    # ★ユマの「4つの名前」を1ミリも違わずに使うよ！！
     client = tweepy.Client(
         consumer_key=os.environ.get("API_KEY"),
         consumer_secret=os.environ.get("API_SECRET"),
@@ -44,7 +47,7 @@ def sine_wave_bot():
     while True:
         material = get_wiki_material()
         txt = make_markov_text(material)
-        post = txt if txt else "波が静かだ"
+        post = txt if txt else "波が静かだ……っ！"
         
         try:
             client.create_tweet(text=post)
@@ -55,7 +58,7 @@ def sine_wave_bot():
         time.sleep(3600)
 
 @app.route('/')
-def home(): return "正弦波くん、名前も完璧モード……っ！"
+def home(): return "正弦波くん、日本語文法（カオス）モード……っ！"
 
 if __name__ == "__main__":
     t = threading.Thread(target=sine_wave_bot)
